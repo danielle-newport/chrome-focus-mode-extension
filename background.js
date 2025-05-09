02e394a94e3d4f41016af50bdec563a96e184e64
danielle-newport <daniellenewport98@gmail.com>
2025-05-09 15:14:15 -0500

    Add script for inserting and removing page styling

--- background.js
+++ background.js
@@ -1,6 +1,20 @@
 const extensions = 'https://developer.chrome.com/docs/extensions';
 const webstore = 'https://developer.chrome.com/docs/webstore';
 
+if (nextState === "ON") {
+    // Insert the CSS file when the user turns the extension on
+    await chrome.scripting.insertCSS({
+        files: ["focus-mode.css"],
+        target: { tabId: tab.id },
+    });
+} else if (nextState === "OFF") {
+    // Remove the CSS file when the user turns the extension off
+    await chrome.scripting.removeCSS({
+        files: ["focus-mode.css"],
+        target: { tabId: tab.id },
+    });
+}
+
 chrome.runtime.onInstalled.addListener(() => {
     chrome.action.setBadgeText({
         text: "OFF",
